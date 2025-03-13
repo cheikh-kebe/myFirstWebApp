9441408544bcb0f3ae4bd62d28674afbb501e285
Cheikh K. <81985121+cheikh-kebe@users.noreply.github.com>
2025-03-13 16:34:29 +0100

    Add LoginController

---
+++ src/main/java/com/learning/springboot/my_first_web_app/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.learning.springboot.my_first_web_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import com.learning.springboot.my_first_web_app.service.AuthenticationService;
+
+@Controller
+public class LoginController {
+
+    private AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService){
+        super();
+        this.authenticationService = authenticationService;
+    }
+
+    @GetMapping("login")
+        public String goToLoginPage(Model model, ModelMap modelMap){
+        model.addAttribute("title", "Welcome to the login page!");
+        return "login";
+    }
+
+    @PostMapping("login")
+        public String goToWelcomePage(
+            @RequestParam String name,
+            @RequestParam String password,
+            ModelMap modelMap, Model model){
+
+        if (authenticationService.authenticate(name, password)) {
+            modelMap.put("name", name);
+            model.addAttribute("name", name);
+        }
+        return "welcome";
+    }
+}
