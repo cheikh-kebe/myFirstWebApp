476317afdd4307b92574ac17695e9052ec2c0e01
Cheikh K. <81985121+cheikh-kebe@users.noreply.github.com>
2025-03-13 16:34:02 +0100

    Add TodoService class

---
+++ src/main/java/com/learning/springboot/my_first_web_app/service/TodoService.java
@@ -0,0 +1,50 @@
+package com.learning.springboot.my_first_web_app.service;
+
+import com.learning.springboot.my_first_web_app.model.Todo;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Iterator;
+
+public class TodoService {
+
+    private static List<Todo> todos = new ArrayList<>();
+    
+    // Private constructor to prevent instantiation
+    private TodoService() {
+    }
+
+    static {
+        todos.add(new Todo(1, "cheikh", "learn spring boot", LocalDate.of(2025, 1, 10), false));
+        todos.add(new Todo(2, "wassim", "learn Java", LocalDate.of(2025, 3, 12), false));
+        todos.add(new Todo(3, "cheikh", "learn spring boot", LocalDate.of(2025, 2, 10), false));
+    }
+    
+    public static List<Todo> findAll() {
+        return todos;
+    }
+    
+    // Get todos by username
+    public static List<Todo> findByUsername(String username) {
+        List<Todo> userTodos = new ArrayList<>();
+        for (Todo todo : todos) {
+            if (todo.getUsername().equals(username)) {
+                userTodos.add(todo);
+            }
+        }
+        return userTodos;
+    }
+    
+    public static boolean deleteById(int id) {
+        Iterator<Todo> iterator = todos.iterator();
+        while (iterator.hasNext()) {
+            Todo todo = iterator.next();
+            if (todo.getId() == id) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+}
